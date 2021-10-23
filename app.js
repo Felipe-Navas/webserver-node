5eb1b9136835e801fa7e239e95c27d63a45458c5
Felipe Navas <felipenavas.itec@gmail.com>
2021-10-23 13:02:48 -0300

    Test usermail

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
-
 const app = express();
 const port = process.env.PORT;
 
@@ -16,11 +15,12 @@ hbs.registerPartials( __dirname + '/views/partials');
 // de la carpeta 'public
 app.use(express.static('public'));
 
+
 app.get('/', function (req, res) {
   // Este render esta levantando el archivo /views/home.hbs
   res.render('home', {
     titulo: 'Webserver con express',
-    autor: 'Felipao',
+    autor: 'Felipe Navas',
   });
 });
 
@@ -28,7 +28,7 @@ app.get('/generic', function (req, res) {
   // Este render esta levantando el archivo /views/generic.hbs
   res.render('generic', {
     titulo: 'Webserver con express',
-    autor: 'Felipao',
+    autor: 'Felipe Navas',
   });
 });
 
@@ -36,7 +36,7 @@ app.get('/elements', function (req, res) {
   // Este render esta levantando el archivo /views/elements.hbs
   res.render('elements', {
     titulo: 'Webserver con express',
-    autor: 'Felipao',
+    autor: 'Felipe Navas',
   });
 });
 
@@ -44,11 +44,10 @@ app.get('*', function (req, res) {
   // Este render esta levantando el archivo /views/error_404.hbs
   res.render('error_404', {
     titulo: 'Webserver con express',
-    autor: 'Felipao',
+    autor: 'Felipe Navas',
   });
   });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
-
